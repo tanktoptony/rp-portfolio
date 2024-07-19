a9aeb1a5752f64555ae0cff5f7be03b1d7b2bc58
tanktoptony <anthony.jarina@gmail.com>
2024-07-19 12:39:10 -0500

    all links currently working - home, blog, projects - navbar created

--- personal_portfolio/views.py
+++ personal_portfolio/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, you are at the homepage.")
+    return render(request, 'index.html')
